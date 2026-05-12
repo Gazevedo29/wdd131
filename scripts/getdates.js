f8ae1c131f3cc1ec7cbb6835b755c2d834af4e3d
Gazevedo <gazevedo@byupathway.edu>
2026-05-12 14:24:13 +0200

    primeiro commit

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+//  ano atual para o copyright
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentYear;
+
+// data da última modificação do documento
+document.getElementById('lastModified').textContent = document.lastModified;
